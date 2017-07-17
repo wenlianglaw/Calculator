8ddbff708dddffbfd9ca1c9ce1c1f26335b36e2c
William Liu <wenlianglaw@gmail.com>
2017-07-17 18:10:42 -0400

    Update main.cpp

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -57,6 +57,50 @@ double F(char *expr, char** s) {
 	}
 	return ret;
 }
+
+#if 0
+double E(char *expr, char** s) {
+	double T(char *expr, char** s);
+	double ret = 0.0F;
+	if (!**s) return ret;
+	ret = T(expr, s);
+	while (*s) {
+		if (**s == '+' || **s == '-') {
+			(*s)++;
+			ret += *(*s - 1) == '+' ? T(expr, s) : -T(expr, s);
+		}
+		else if (**s == '(')
+			ret = E(expr, s);
+		else break;
+	}
+	return ret;
+}
+
+double T(char *expr, char** s) {
+	double F(char *expr, char** s);
+	double ret;
+	ret = F(expr, s);
+	if (**s == '*' || **s == '/') {
+		(*s)++;
+		ret *= *(*s - 1) == '*' ? F(expr, s) : 1 / F(expr, s);
+	}
+	return ret;
+}
+
+double F(char *expr, char** s) {
+	if (!**s) return 1.0F;
+	double ret = 1.0F;
+	if (**s == '(') {
+		(*s)++;
+		ret = E(expr, s);
+	}
+	if (**s == ')') (*s)++;// do nothing
+	else if (**s >= '0' && **s <= '9' || **s == '+' || **s == '-') {
+		ret = strtod(*s, s);
+	}
+	return ret;
+}
+#endif
 int main() {
 	//char *str = "3*(4+5)*6";	
 	char *str = "1+2*(4+3)";
