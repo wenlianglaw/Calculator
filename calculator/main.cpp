6378b67cf3f12d7c21711e756f3c199817def9bf
William Liu <wenlianglaw@gmail.com>
2023-07-15 01:18:12 -0700

    Update main.cpp
    Removed necessary handling of "(" in function E and F

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
+void trim_space(char *s) {
+  char *p = s;
+  while (*s) {
+    if (*s == ' ') s++;
+    else *p++ = *s++;
+  }
+  *p = '\0';
+}
 
 /*
 expr is the expression
@@ -8,57 +17,60 @@ s is the start character's address, *s is the start character
 E -> T +- T | T
 T -> F * / F | F
 F -> (E) | num
-
+-----------------------
+or
+E -> T +- E | T
+T -> F * / T | F
+F -> (E) | num
 LWL
 wenlianglaw@gmail.com
 NYC
 2015-09-09
+
+2023 updated removed some unnecessary code, such as handling "(" in E and F.
 */
-double E(char *expr,char** s) {
-	double T(char *expr, char** s);
-	double ret = 0.0F;
-	if (!**s) return ret;
-	ret = T(expr, s);
-	while (**s) {
-		char op = *(*s)++;
-		if (op == '+') ret += T(expr, s);
-		else if (op == '-') ret -= T(expr, s);
-		else if (op == ')') break;
-		else (*s)--;
-	}
-	return ret;
+
+#define recursive
+#ifndef recursive
+double E(char *expr, char **s) {
+  double T(char *expr, char **s);
+  double ret = 0.0F;
+  if (!**s) return ret;
+  ret = T(expr, s);
+  while (**s == '+' || **s == '-') {
+    (*s)++;
+    ret += *(*s - 1) == '+' ? T(expr, s) : -T(expr, s);
+  }
+  return ret;
 }
 
-double T(char *expr, char** s) {
-	double F(char *expr, char** s);
-	double ret;
-	if (!**s) ret;
-	ret = F(expr, s);
-	while ( **s == '*' || **s == '/' ) {
-		char op = *(*s)++;		
-		if (op == '*') ret *= F(expr, s);
-		else if (op == '/') ret /= F(expr, s);
-		else (*s)--;
-	}
-	return ret;
+double T(char *expr, char **s) {
+  double F(char *expr, char **s);
+  double ret;
+  ret = F(expr, s);
+  while (**s == '*' || **s == '/') {
+    (*s)++;
+    ret *= *(*s - 1) == '*' ? F(expr, s) : 1 / F(expr, s);
+  }
+  return ret;
 }
 
-double F(char *expr, char** s) {
-	if (!**s) return 1.0F;
-	char ch = **s;
-	double ret = 1.0F;
-	if (**s == '(') {
-		(*s)++;
-		ret = E(expr, s);
-	} 
-	else if (**s == ')');// do nothing
-	else if (**s >= '0' && **s <= '9' || **s =='+' || **s == '-') {
-		ret = strtod(*s, s);
-	}
-	return ret;
+double F(char *expr, char **s) {
+  if (!**s) return 1.0F;
+  double ret = 1.0F;
+  if (**s == '(') {
+    (*s)++;
+    ret = E(expr, s);
+  }
+  if (**s == ')')
+    (*s)++;  // do nothing
+  else if (**s >= '0' && **s <= '9' || **s == '+' || **s == '-') {
+    ret = strtod(*s, s);
+  }
+  return ret;
 }
 
-#ifdef recursive
+#else  // ifdef recursive
 double E(char *expr, char** s) {
 	double T(char *expr, char** s);
 	double ret = 0.0F;
@@ -69,8 +81,6 @@ double E(char *expr, char** s) {
 			(*s)++;
 			ret += *(*s - 1) == '+' ? T(expr, s) : -T(expr, s);
 		}
-		else if (**s == '(')
-			ret = E(expr, s);
 		else break;
 	}
 	return ret;
@@ -85,10 +95,7 @@ double T(char *expr, char** s) {
 			(*s)++;
 			ret *= *(*s - 1) == '*' ? F(expr, s) : 1 / F(expr, s);
 		}
-		else if (**s == '(')
-			ret = F(expr, s);
 		else break;
-
 	}
 	return ret;
 }
@@ -107,13 +114,14 @@ double F(char *expr, char** s) {
 	return ret;
 }
 #endif
-int main() {
-	//char *str = "3*(4+5)*6";	
-	char *str = "1+2*(4+3)";
-	char *pstr = str;
-	double d = E(str,&pstr);
-	printf("%lf\n", d);
 
-	getchar();
-	return 0;
+int main() {
+  // char *str = "3*(4+5)*6";
+  char str[] = "(3*2) + 1.5 + 1.5 + 4 * (3+2) - (3*2) - (2+((2))) / (2)";
+  char *pstr = str;
+  trim_space(str);
+  double d = E(str, &pstr);
+  printf("%lf\n", d);
+
+  return 0;
 }
